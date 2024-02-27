e83004b8b502efeb258e9249a54027a79db3e9d4
CJ-arg <janoncarlos@gmail.com>
2024-02-27 17:13:16 -0300

    navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,12 +2,9 @@ const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
       <span className="flex bg-red-500 bg-opacity-10 p-1 m-1 rounded ">
-        {" "}
         Home
       </span>
-
       <div className="flex flex-1">
-        {" "}
         <a
           className="mr-2 bg-red-500 bg-opacity-10 p-1 m-1 rounded "
           href="1/about"
