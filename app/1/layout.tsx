50805fd4abb5d09d45b6c422494e8058bb0fd901
CJ-arg <janoncarlos@gmail.com>
2024-02-27 16:51:20 -0300

    navbar

--- app/1/layout.tsx
+++ app/1/layout.tsx
@@ -1,13 +1,18 @@
+import Navbar from "@/components/navbar/Navbar";
+
 export default function Multi1Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex flex-col items-center p-24">
+    <> 
+    <Navbar/>
+     <main className="flex flex-col items-center p-24">
       <span className="text-lg">Pagina Portfolio Page</span>
 
       {children}
     </main>
-  );
+  );</>
+  
 }
